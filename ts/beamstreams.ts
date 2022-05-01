b840081d83ce04a4ba6afbb83ff273843b86d484
urielch <uriel.chemouni@gmail.com>
2022-05-01 22:36:41 +0300

    fix teeBalancer export

--- ts/beamstreams.ts
+++ ts/beamstreams.ts
@@ -23,6 +23,7 @@ import bindings from 'bindings';
 import calcStats from './calcStats';
 import createBeamReadableStream from './createBeamReadableStream';
 import createBeamWritableStream from './createBeamWritableStream';
+import teeBalancer from './teeBalancer';
 //import { BeamcoderType } from './types/BeamcoderType';
 
 const beamcoder = bindings('beamcoder') as any;// BeamcoderType;
@@ -228,75 +229,6 @@ function parallelBalancer(params, streamType, numStreams) {
   };
 
   return readStream;
-}
-
-function teeBalancer(params, numStreams) {
-  let resolvePush = null;
-  const pending = [];
-  for (let s = 0; s < numStreams; ++s)
-    pending.push({ frames: null, resolve: null, final: false });
-  
-  const pullFrame = async index => {
-    return new Promise(resolve => {
-      if (pending[index].frames) {
-        resolve({ value: pending[index].frames, done: false });
-        Object.assign(pending[index], { frames: null, resolve: null });
-      } else if (pending[index].final)
-        resolve({ done: true });
-      else
-        pending[index].resolve = resolve;
-
-      if (resolvePush && pending.every(p => null === p.frames)) {
-        resolvePush();
-        resolvePush = null;
-      }
-    });
-  };
-
-  const readStreams = [];
-  for (let s = 0; s < numStreams; ++s)
-    readStreams.push(new Readable({
-      objectMode: true,
-      highWaterMark: params.highWaterMark ? params.highWaterMark || 4 : 4,
-      read() {
-        (async () => {
-          const start = process.hrtime();
-          const reqTime = start[0] * 1e3 + start[1] / 1e6;
-          const result: any = await pullFrame(s);
-          if (result.done)
-            this.push(null);
-          else {
-            result.value.timings[params.name] = { reqTime: reqTime, elapsed: process.hrtime(start)[1] / 1000000 };
-            this.push(result.value);
-          }
-        })();
-      },
-    }));
-
-  (readStreams as any).pushFrames = frames => {
-    return new Promise(resolve => {
-      pending.forEach((p, index) => {
-        if (frames.length)
-          p.frames = frames[index].frames;
-        else
-          p.final = true;
-      });
-
-      pending.forEach(p => {
-        if (p.resolve) {
-          if (p.frames) {
-            p.frames.timings = frames.timings;
-            p.resolve({ value: p.frames, done: false });
-          } else if (p.final)
-            p.resolve({ done: true });
-        }
-        Object.assign(p, { frames: null, resolve: null });
-      });
-      resolvePush = resolve;
-    });
-  };
-
-  return readStreams;
 }
 
 function transformStream(params, processFn, flushFn, reject) {
