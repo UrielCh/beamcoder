7cdad869e09776e16929328c4bdb34fbcea1db8f
urielch <uriel.chemouni@gmail.com>
2022-05-05 21:51:39 +0300

    update test

--- test/mp4FullTest.ts
+++ test/mp4FullTest.ts
@@ -9,11 +9,13 @@ test('recompress mp4', async t => {
   async function run() {
     const mediaFile = '../big_buck_bunny_1080p_h264.mov'
     if (!fs.existsSync(mediaFile)) {
-      console.log(`${mediaFile} is missing Downloading if now`);
+      console.log(`${mediaFile} is missing Downloading it, now using torrent magnet link.`);
       const client = new WebTorrent()
       const magnetURI = 'magnet:?xt=urn:btih:P42GCLQPVRPHWBI3PC67CBQBCM2Q5P7A&dn=big_buck_bunny_1080p_h264.mov&xl=725106140&tr=http%3A%2F%2Fblender.waag.org%3A6969%2Fannounce'
       await new Promise<void>((done) => {
-        client.add(magnetURI, {path: '..'}, function (torrent) {
+        client.add(magnetURI, { path: '..' }, function (torrent) {
+          let len = 0;
+          let ready = '0';
           // Got torrent metadata!
           console.log('Client is downloading:', torrent.infoHash)
           torrent.files.forEach(function (file) {
@@ -23,7 +25,14 @@ test('recompress mp4', async t => {
           })
           torrent.on("done", () => done());
           torrent.on("wire", (wire, addr) => console.log(`wire ${wire} addr: ${addr}`));
-          torrent.on('download', (bytes: number) => console.log(`${bytes} downloaded`));
+          torrent.on('download', (bytes: number) => {
+            len += bytes;
+            let ready2 = (len / (1024 * 1024)).toFixed(2);
+            if (ready != ready2) {
+              ready = ready2;
+              console.log(`${ready2} downloaded to ${mediaFile}`);
+            }
+          });
         })
       })
       console.log(`${mediaFile} Downloaded`);
@@ -33,7 +42,7 @@ test('recompress mp4', async t => {
       console.log(`${mediaFile} still missing`);
       return;
     }
-  
+
     const src = mediaFile;
     const sumSrc = await md5File(src);
 
@@ -83,7 +92,7 @@ test('recompress mp4', async t => {
             channel_layout: 'mono'
           }
         }]
-      }, ],
+      },],
       out: {
         formatName: 'mp4',
         url: 'file:temp.mp4'
@@ -99,7 +108,7 @@ test('recompress mp4', async t => {
 
     if (os.platform() === 'darwin') {
       t.equal(sumDest, '784983c8128db6797be07076570aa179', 'dest File have incorrect md5sum');
-    } else  {
+    } else {
       t.equal(sumDest, 'f08742dd1982073c2eb01ba6faf86d63', 'dest File have incorrect md5sum');
     }
   }
